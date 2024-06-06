d49acc5480775faf6bc954d6df56b3cd8f5f3fe3
diogobc12 <diogo.campos122005@gmail.com>
2024-06-06 15:31:40 +0100

    Primeiro pull request Diogo Campos

--- teste.c
+++ teste.c
@@ -9,4 +9,22 @@ int main()
 	printf("A minha primeirissima alteração\n");
 
 	printf("A minha quarta alteração\n");
+	
+
+    char resposta[10];
+
+    printf("Estou passado? (sim/nao): ");
+    scanf("%s", resposta);
+
+    if (strcmp(resposta, "sim") == 0) {
+        printf("Negócio fechado com sucesso\n");
+    }
+    else if (strcmp(resposta, "nao") == 0) {
+        printf(":( Adeus\n");
+    }
+    else {
+        printf("Resposta inválida.\n");
+    }
+
+    return 0
 }
