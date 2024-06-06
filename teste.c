7635aca9e1e72039ca9343ec4640720afb9b14c2
mcllek <diogomigrocha@gmail.com>
2024-06-06 15:18:20 +0100

    Primeiro push feito por Diogo Rocha

--- teste.c
+++ teste.c
@@ -2,12 +2,18 @@
  * O meu primeiro projeto no git.
  */
 #include <stdio.h>
+#include <locale.h>
 
 int main()
 {
+	setlocale(LC_CTYPE, "pt_PT");
+
 	printf("O meu primeiro projeto no git\n");
 	printf("A minha primeirissima alteração\n");
 
 	printf("A minha quarta alteração\n");
-	printf("Ola sou, Diogo Rocha.");
+	
+	printf("Primeiro push feito por Diogo Rocha.\n");
+
+	return 0;
 }
